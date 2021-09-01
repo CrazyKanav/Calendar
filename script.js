130f0150011cb6161234fb42e7bb20bf26d40391
Kanav Baid <kanav.baid@outlook.com>
2021-09-01 14:42:42 +0545

    Fixed some bugs

--- script.js
+++ script.js
@@ -4,18 +4,13 @@ $("#calendar").evoCalendar();
     {
       id: '09nk7Ts',
       name: "My Birthday",
-      date: "September/1/2021",
+      date: "Feburary/6/2021",
+      description: "Something text",
       type: "birthday",
       everyYear: true
     }
     // Add more
   ]);
-
-  $("#calendar").on('selectDate', function() {
+  $("#calendar").on('selectEvent', function() {
     location.href='http://www.google.com'; // Website Link
   });
-  
-  $("#calendar").on('selectEvent', new_fart() {
-    location.href='http://www.google.com'; // Website Link
-  });
-
